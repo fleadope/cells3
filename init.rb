febb3f43ccf6d20c81dd36e01f492533022d2c70
mlomnicki <michal@lomnicki.com.pl>
2009-11-11 23:58:41 +0100

    use config methods instead of initializer.
    Rails 3 boots with cells without exceptions.

--- init.rb
+++ init.rb
@@ -42,7 +42,7 @@ Cell::Base.add_view_path "app/cells/layouts"
 # process cells in plugins ("engine-cells").
 # thanks to Tore Torell for making me aware of the initializer instance here:
 config.after_initialize do
-  initializer.loaded_plugins.each do |plugin|
+  config.loaded_plugins.each do |plugin|
     engine_cells_dir = File.join([plugin.directory, "app/cells"])
     next unless plugin.engine?
     next unless File.exists?(engine_cells_dir)
